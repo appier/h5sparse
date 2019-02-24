101b881fad246373a1db1c4b679c8823298655f3
Ian Lin <ian.yian.lin@gmail.com>
2019-02-24 17:25:49 +0800

    upgrade python and add flake8 in tox

--- h5sparse/__init__.py
+++ h5sparse/__init__.py
@@ -1,4 +1,4 @@
 import pkg_resources
-from .h5sparse import Group, File, Dataset
+from .h5sparse import Group, File, Dataset  # noqa: F401
 
 __version__ = pkg_resources.get_distribution("h5sparse").version
